256cf69d3f5eb93ce3f6df7a5b5dffd4d850b19f
Your Name <you@example.com>
2017-04-16 11:50:47 +0100

    Replaced the websock client code with the SPSSub gem implementation. Which makes it easier to read and maintain.

--- lib/spsclient_m2m.rb
+++ lib/spsclient_m2m.rb
@@ -6,106 +6,88 @@
 require 'logger'
 require 'polyrex'
 require 'spstrigger_execute'
-require 'websocket-eventmachine-client'    
+require 'sps-sub'    
 
 
 class SPSClientM2M
 
-  def initialize(rsc, reg, sps_keywords_url, px_url, logfile: nil, \
-                                          sps: {host: 'sps', port: '59000'})
+  def initialize(rsc, reg, keywords, px_url, logfile: nil, \
+                             sps_host: 'sps', sps_port: '59000', topic: '#')
           
     @rsc = rsc
     @log = Logger.new(logfile,'daily') if logfile
 
-    @sps_address = "%s:%s" % [sps[:host], sps[:port]]
+    @sps_address = "%s:%s" % [sps_host, sps_port]
+    @topic = topic
+    
+    @sps = SPSSub.new host: sps_host, port: sps_port
     
     px = Polyrex.new px_url
 
-    @ste = SPSTriggerExecute.new sps_keywords_url, reg, px, logfile: 'ste.log'
+    @ste = SPSTriggerExecute.new keywords, reg, px, logfile: 'ste.log'
 
   end
   
   def run()
-
+ 
     rsc = @rsc
     ste = @ste
-    sps_address = @sps_address
     
-    EM.run do
+    @sps.subscribe(topic: @topic) do |raw_message, topic|
 
-      ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://' + sps_address)
+      puts "[%s] SPS M2M kywrd lstnr INFO %s: %s" % \
+                      [Time.now.strftime("%D %H:%M"), topic, raw_message]
 
-      ws.onopen do
-        puts "Client connected"
+      a = ste.mae topic, raw_message
+      log 'a: ' + a.inspect
+
+      # obj is the DRb object, r is the result from find_match, 
+      # a is the Dynarex lookup array
+
+      if a.length > 0 then
+        
+        h = {
+        
+          rse: ->(x, rsc){
+          
+            job = x.shift[/\/\/job:(.*)/,1]                  
+            package_path = x.shift 
+            package = package_path[/([^\/]+)\.rsf$/,1]
+            
+            log "job: %s path: %s package: %s" % [job, package_path, package]
+            log 'foo: ' + rsc.r.hello
+            rsc.run_job package, job, {}, args=x, package_path: package_path
+          }, 
+          sps: ->(x, rsc){ @sps.notice x },
+          ste: ->(x, rsc){ log 'before ste run'; ste.run x }
+        }
+
       end
 
-      ws.onmessage do |msg, type|
-
-        log 'msg : ' + msg.inspect
-        topic, raw_message = msg.split(/\s*:\s*/,2)
-        puts "[%s] SPS M2M kywrd lstnr INFO %s: %s" % \
-                        [Time.now.strftime("%D %H:%M"), topic, raw_message]
-
-        a = ste.mae topic, raw_message
-        log 'a: ' + a.inspect
-
-        # obj is the DRb object, r is the result from find_match, 
-        # a is the Dynarex lookup array, ws is the websocket.
-
-        if a.length > 0 then
-          
-          h = {
-          
-            rse: ->(x){
-            
-              job = x.shift[/\/\/job:(.*)/,1]                  
-              package_path = x.shift 
-              package = package_path[/([^\/]+)\.rsf$/,1]
-              
-              log "job: %s path: %s package: %s" % [job, package_path, package]
-              rsc.run_job package, job, {}, args=x, package_path: package_path
-            }, 
-            sps: ->(x){ ws.send x },
-            ste: ->(x){ ste.run x }
-          }
-
-        end
+      EM.defer do          
         
-        EM.defer do  
+        a.each do |type, x| 
           
-          a.each do |type, x| 
+          Thread.new do
             
             begin
-              h[type].call x
+              h[type].call x, rsc
             rescue
               warning =  'SPSClientM2M warning: ' + ($!).inspect
               puts warning
               log warning
             end
             
-          end
+          end # /thread
           
-        end
-      end
-
-      ws.onclose do
-        puts "Client disconnected"
-      end
-
-      EventMachine.next_tick do
-        ws.send 'subscribe to topic: #'
-        ws.send 'rse_info: spsclient_m2m connected'
-      end
+        end # /each
+        
+      end      
       
-      EventMachine.error_handler{ |e|
-        puts "Error raised during event loop: #{e.message}"
-      
-      }
-
-    end
-  
+    end        
+    
   end
-  
+
   private
   
   def log(s)
--- spsclient_m2m.gemspec
+++ spsclient_m2m.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'spsclient_m2m'
-  s.version = '0.2.1'
+  s.version = '0.3.0'
   s.summary = 'The SPSClient_M2M gem is designed to run as a service with the RSF_Services gem and the dws-registry gem or the remote_dwsregistry gem.'
   s.authors = ['James Robertson']
-  s.add_runtime_dependency('polyrex', '~> 1.0', '>=1.1.1')  
+  s.add_runtime_dependency('polyrex', '~> 1.1', '>=1.1.12')  
   s.add_runtime_dependency('spstrigger_execute', '~> 0.4', '>=0.4.6')
-  s.add_runtime_dependency('websocket-eventmachine-client', '~> 1.0', '>=1.1.0')
+  s.add_runtime_dependency('sps-sub', '~> 0.3', '>=0.3.4')
   s.files = Dir['lib/spsclient_m2m.rb']
   s.signing_key = '../privatekeys/spsclient_m2m.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/spsclient_m2m'
 end
